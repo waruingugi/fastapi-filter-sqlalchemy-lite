5411ded835aa873123bc140efb730ed557256b5e
waruingugi <waruingugientp@gmail.com>
2023-03-03 14:06:36 +0300

    Minor addition in setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setuptools.setup(
     version = "0.1.0",
     description = "A simple FastAPI filter for SQLAlchemy",
     authors = ["Arthur Rio <arthur.rio44@gmail.com>", "Warui <warui@marketforce360.com>"],
-packages = ["fastapi_sqlalchemy_filter"]
+    packages = ["fastapi_sqlalchemy_filter"],
 )
